b1ad68c5ccc7d8abfc0c3f706e2cef1f17e59578
Jan Cajthaml <jan.cajthaml@gmail.com>
2019-07-24 07:41:12 +0200

    Update crc.go

--- crc.go
+++ crc.go
@@ -1,12 +1,13 @@
 package crc8
 
+// Checksum returns CRC8 checksum for given parameters
 func Checksum(data []byte, poly uint8, init uint8, xorout uint8) uint8 {
 	var crc uint8 = init
 	var bit uint8
 
 	for _, item := range data {
-		for j := uint16(0x0080); j != 0; j >>= 1 {
-			if (uint16(item) & j) != 0 {
+		for j := byte(0x0080); j != 0; j >>= 1 {
+			if (item & j) != 0 {
 				bit = (crc & 0x80) ^ 0x80
 			} else {
 				bit = crc & 0x80
