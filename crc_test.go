62d3f5041e50d8d612174870ef125f1c7f41cd4b
Jan Cajthaml <jan.cajthaml@gmail.com>
2019-07-24 07:43:34 +0200

    Update crc_test.go

--- crc_test.go
+++ crc_test.go
@@ -22,67 +22,67 @@ func TestNormalized(t *testing.T) {
 
 	input := []byte("abcdefgh")
 
-	t.Log("CRC8")
+	t.Log("CRC8/CRC-8")
 	{
 		AssetEqual(t, 0xCB, Checksum(input, 0x07, 0x00, 0x00))
 	}
 
-	t.Log("CRC8_SAE_J1850")
+	t.Log("CRC8/SAE-J1850")
 	{
 		AssetEqual(t, 0xD7, Checksum(input, 0x1D, 0xFF, 0xFF))
 	}
 
-	t.Log("CRC8_SAE_J1850_ZERO")
+	t.Log("CRC8/SAE-J1850-ZERO")
 	{
 		AssetEqual(t, 0x3E, Checksum(input, 0x1D, 0x00, 0x00))
 	}
 
-	t.Log("CRC8_8H2F")
+	t.Log("CRC8/8H2F")
 	{
 		AssetEqual(t, 0x54, Checksum(input, 0x2F, 0xFF, 0xFF))
 	}
 
-	t.Log("CRC8_CDMA2000")
+	t.Log("CRC8/CDMA2000")
 	{
 		AssetEqual(t, 0xF7, Checksum(input, 0x9B, 0xFF, 0x00))
 	}
 
-	//t.Log("CRC8_DARC")
+	//t.Log("CRC8/DARC")
 	//{
 	//AssetEqual(t, 0x62, Checksum(input, 0x39, 0x00, 0x00, true, true))
 	//}
 
-	t.Log("CRC8_DVB_S2")
+	t.Log("CRC8/DVB-S2")
 	{
 		AssetEqual(t, 0x62, Checksum(input, 0xD5, 0x00, 0x00))
 	}
 
-	//t.Log("CRC8_EBU")
+	//t.Log("CRC8/EBU")
 	//{
 	//AssetEqual(t, 0x41, Checksum(input, 0x1D, 0xFF, 0x00, true, true))
 	//}
 
-	t.Log("CRC8_ICODE")
+	t.Log("CRC8/ICODE")
 	{
 		AssetEqual(t, 0x96, Checksum(input, 0x1D, 0XFD, 0x00))
 	}
 
-	t.Log("CRC8_ITU")
+	t.Log("CRC8/ITU")
 	{
 		AssetEqual(t, 0x9E, Checksum(input, 0x7, 0X00, 0x55))
 	}
 
-	//t.Log("CRC8_MAXIM")
+	//t.Log("CRC8/MAXIM")
 	//{
 	//AssetEqual(t, 0x92, Checksum(input, 0x31, 0X00, 0x00, true, true))
 	//}
 
-	//t.Log("CRC8_ROHC")
+	//t.Log("CRC8/ROHC")
 	//{
 	//AssetEqual(t, 0x15, Checksum(input, 0x7, 0XFF, 0x0))
 	//}
 
-	//t.Log("CRC8_ITU")
+	//t.Log("CRC8/ITU")
 	//{
 	//AssetEqual(t, 0x3D, Checksum(input, 0x9B, 0X00, 0x00))
 	//}
